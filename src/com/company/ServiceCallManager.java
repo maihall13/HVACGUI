8d0faa15714b242a0d149e7b98b52fc4a3997c15
maihall13 <maiahall1396@gmail.com>
2017-03-19 16:00:59 -0500

    Updated ServiceCallManager class to include features from the WaterHeater class

--- src/com/company/ServiceCallManager.java
+++ src/com/company/ServiceCallManager.java
@@ -24,7 +24,8 @@ public class ServiceCallManager {
     String[] addCallOptions = {
             "1. Add service call for furnace",
             "2. Add service call for AC unit",
-            "3. Return to main menu" };
+            "3. Add service call for water heater",
+            "4. Return to main menu" };
 
 
     /* Constructor sets up the two lists, the UserInput object, and starts the main menu */
@@ -98,6 +99,9 @@ public class ServiceCallManager {
                 addACServiceCall();
             }
             else if (choice == 3) {
+                addWaterHeaterCall();
+            }
+            else if(choice == 4){
                 return;
             }
             else {
@@ -130,6 +134,18 @@ public class ServiceCallManager {
         CentralAC ac = new CentralAC(address, problem, new Date(), model);
         todayServiceCalls.add(ac);
         System.out.println("Added the following AC unit to list of calls:\n" + ac);
+    }
+
+    //Add water heater to service call manager
+    private void addWaterHeaterCall() {
+
+        String address = Input.getStringInput("Enter address of water heater");
+        String problem = Input.getStringInput("Enter description of problem");
+        Double age = Input.getPositiveDoubleInput("Enter age of water heater");
+
+        WaterHeater wh = new WaterHeater(address, problem, new Date(), age);
+        todayServiceCalls.add(wh);
+        System.out.println("Added the following AC unit to list of calls:\n" + wh);
     }
 
 
@@ -196,5 +212,4 @@ public class ServiceCallManager {
             System.out.println(call + "\n");
         }
     }
-
 }
